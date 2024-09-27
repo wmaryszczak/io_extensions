5dafb06358b250f9a659f6582153bbe42d7cd037
Wojciech Maryszczak <wojciech.maryszczak@anixe.pl>
2024-09-27 14:51:47 +0200

    Add injecting custom deserializer for each JSONL line

--- WMA.IOExtensions/Json/MultiContentJsonReaderOptions.cs
+++ WMA.IOExtensions/Json/MultiContentJsonReaderOptions.cs
@@ -3,25 +3,25 @@ using System.Text.Json;
 
 namespace WMA.IOExtensions.Json
 {
-  public enum ObjectScanStrategyEnum
-  {
-    /// <summary>
-    /// Use Utf8JsonReader.TrySkip method to determine boundaries for single json object to be deserialized.
-    /// So each json object does not have to be in separated line
-    /// </summary>
-    JsonTrySkip = 0,
-    /// <summary>
-    /// Use new line char separator to determine boundaries for single json object to be deserialized.
-    /// So each json object shoud be in separated line
-    /// </summary>
-    NewLine
-  }
+    public enum ObjectScanStrategyEnum
+    {
+        /// <summary>
+        /// Use Utf8JsonReader.TrySkip method to determine boundaries for single json object to be deserialized.
+        /// So each json object does not have to be in separated line
+        /// </summary>
+        JsonTrySkip = 0,
 
+        /// <summary>
+        /// Use new line char separator to determine boundaries for single json object to be deserialized.
+        /// So each json object shoud be in separated line
+        /// </summary>
+        NewLine,
+    }
 
-  public class MultiContentJsonReaderOptions
-  {
-    public ObjectScanStrategyEnum ObjectScanStrategy { get; set; }
-    public StreamPipeReaderOptions? StreamPipeReaderOptions { get; set; }
-    public JsonSerializerOptions? JsonSerializerOptions { get; set; }
-  }
+    public class MultiContentJsonReaderOptions
+    {
+        public ObjectScanStrategyEnum ObjectScanStrategy { get; set; }
+        public StreamPipeReaderOptions? StreamPipeReaderOptions { get; set; }
+        public JsonSerializerOptions? JsonSerializerOptions { get; set; }
+    }
 }
